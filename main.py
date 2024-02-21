90e044db0e78bf4ec1294d9120e37c89503f7726
Serhii Herasymiuk <sgerasimuk07@gmail.com>
2024-02-21 23:27:52 +0200

    Added game

--- main.py
+++ main.py
@@ -1,16 +1,52 @@
-# This is a sample Python script.
+def print_towers(towers):
+    num_disks = max(len(tower) for tower in towers)
+    for level in range(num_disks - 1, -1, -1):
+        for tower in towers:
+            if level < len(tower):
+                print(f"{'*' * tower[level]:^7}", end='')
+            else:
+                print(f"{'|':^7}", end='')
+        print()
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+def move_disk(source, target, towers):
+    if not towers[source]:
+        print("Invalid move! Source tower is empty.")
+        return False
+    elif towers[target] and towers[source][-1] > towers[target][-1]:
+        print("Invalid move! Cannot place a larger disk on top of a smaller one.")
+        return False
+    else:
+        disk = towers[source].pop()
+        towers[target].append(disk)
+        return True
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def tower_of_hanoi(num_disks, num_sticks):
+    towers = [[] for _ in range(num_sticks)]
+    towers[0] = list(range(num_disks, 0, -1))
+
+    while True:
+        print_towers(towers)
+
+        source = int(input("Enter source stick (1, 2, 3): ")) - 1
+        target = int(input("Enter target stick (1, 2, 3): ")) - 1
+
+        if source < 0 or source >= num_sticks or target < 0 or target >= num_sticks:
+            print("Invalid input! Stick number out of range.")
+            continue
+
+        if move_disk(source, target, towers):
+            print("Move successful!")
+        else:
+            print("Move failed. Try again.")
+
+        if towers[-1] == list(range(num_disks, 0, -1)):
+            print("Congratulations! You've solved the Tower of Hanoi!")
+            break
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+if __name__ == "__main__":
+    num_disks = 5
+    num_sticks = 3
+    tower_of_hanoi(num_disks, num_sticks)
